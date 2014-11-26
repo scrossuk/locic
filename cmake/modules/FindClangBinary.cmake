bd6236e805e78ca7e311ccc965f5bc25ca13995b
Stephen Cross <scross@scross.co.uk>
2014-11-26 21:59:32 +0000

    Enabled builds with LLVM-3.3 in .travis.yml and modified FindClangBinary.cmake to use a name of a clang binary (to avoid build system confusion).

--- cmake/modules/FindClangBinary.cmake
+++ cmake/modules/FindClangBinary.cmake
@@ -8,13 +8,30 @@
 # CLANGBINARY_FOUND
 # CLANGBINARY_EXECUTABLE
 
-find_program(CLANGBINARY_EXECUTABLE
-	NAMES clang-3.6 clang-3.5 clang-3.4 clang-3.3 clang
-	DOC "Path to clang executable")
+set(CLANG_BINARY_NAME "" CACHE STRING "Set Clang binary name.")
+
+if(NOT "${CLANG_BINARY_NAME}" STREQUAL "")
+	find_program(CLANGBINARY_EXECUTABLE
+		NAMES ${CLANG_BINARY_NAME}
+		DOC "Path to clang executable"
+	)
+else(NOT "${CLANG_BINARY_NAME}" STREQUAL "")
+	set(CLANG_BINARY_SEARCH_NAMES
+		clang-3.6
+		clang-3.5
+		clang-3.4
+		clang-3.3
+		clang
+	)
+	find_program(CLANGBINARY_EXECUTABLE
+		NAMES ${CLANG_BINARY_SEARCH_NAMES}
+		DOC "Path to clang executable"
+	)
+endif(NOT "${CLANG_BINARY_NAME}" STREQUAL "")
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(ClangBinary
 	"Failed to locate Clang executable"
 	CLANGBINARY_EXECUTABLE
-	)
+)
 
