012ad45de014816577b20099db9eece46b036550
Stephen Cross <scross@scross.co.uk>
2015-03-29 21:56:45 +0100

    Updated version to 1.3, removed internal llvm-abi library (this is now an external project) and added build commands to fetch/build it from its new location.

---
+++ cmake/modules/FindLLVMABI.cmake
@@ -0,0 +1,41 @@
+# - Find llvm-abi support library.
+
+# TODO: provide a way to use a custom build of llvm-abi.
+
+set(LLVMABI_INSTALL_DIR "${PROJECT_BINARY_DIR}/llvm-abi-install")
+set(LLVMABI_INCLUDE_LOCATION "${LLVMABI_INSTALL_DIR}/include")
+set(LLVMABI_LIB_LOCATION "${LLVMABI_INSTALL_DIR}/lib/${CMAKE_STATIC_LIBRARY_PREFIX}llvm-abi${CMAKE_STATIC_LIBRARY_SUFFIX}")
+
+if(NOT TARGET llvm-abi)
+	include(ExternalProject)
+	
+	set(LLVMABI_REPO "https://github.com/scross99/llvm-abi.git")
+	
+	set(LLVMABI_CMAKE_ARGS
+		"-DCMAKE_BUILD_TYPE=${CMAKE_BUILD_TYPE}"
+		"-DCMAKE_INSTALL_PREFIX=${LLVMABI_INSTALL_DIR}"
+	)
+	
+	# Pull llvm-abi from repository and build it.
+	ExternalProject_Add(LLVMABIProject
+		GIT_REPOSITORY "${LLVMABI_REPO}"
+		CMAKE_ARGS ${LLVMABI_CMAKE_ARGS}
+		INSTALL_DIR "${LLVMABI_INSTALL_DIR}"
+	)
+	
+	# Create imported library target.
+	add_library(llvm-abi STATIC IMPORTED GLOBAL)
+	
+	set_target_properties(llvm-abi PROPERTIES
+		IMPORTED_LOCATION "${LLVMABI_LIB_LOCATION}"
+	)
+	
+	add_dependencies(llvm-abi LLVMABIProject)
+	
+	message(STATUS "Building llvm-abi from ${LLVMABI_REPO}")
+endif(NOT TARGET llvm-abi)
+
+set(LLVMABI_FOUND TRUE)
+set(LLVMABI_DEPENDENCIES llvm-abi)
+set(LLVMABI_INCLUDE_DIRS "${LLVMABI_INCLUDE_LOCATION}")
+set(LLVMABI_LIBRARIES llvm-abi)
