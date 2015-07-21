ef470c2e5344ef72ce2461e9a6222b0e5e938d03
Stephen Cross <scross@scross.co.uk>
2015-07-21 19:11:45 +0100

    ref T30: Added __alignmask() and __sizeof() for pointer primitive.

--- lib/CodeGen/Primitives/Ptr.cpp
+++ lib/CodeGen/Primitives/Ptr.cpp
@@ -49,6 +49,12 @@ namespace locic {
 			switch (methodID) {
 				case METHOD_NULL:
 					return ConstantGenerator(module).getNull(genType(module, type));
+				case METHOD_ALIGNMASK: {
+					return genAlignMask(function, type);
+				}
+				case METHOD_SIZEOF: {
+					return genSizeOf(function, type);
+				}
 				case METHOD_COPY:
 				case METHOD_IMPLICITCOPY:
 				case METHOD_DEREF:
