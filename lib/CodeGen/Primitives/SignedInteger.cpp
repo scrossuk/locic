341fff030d2c836cfb06997744e01525e221b3b9
Stephen Cross <scross@scross.co.uk>
2015-07-21 19:12:26 +0100

    ref T30: Added __alignmask() and __sizeof() for signed integer primitives.

--- lib/CodeGen/Primitives/SignedInteger.cpp
+++ lib/CodeGen/Primitives/SignedInteger.cpp
@@ -62,7 +62,11 @@ namespace locic {
 			const auto zero = ConstantGenerator(module).getPrimitiveInt(typeName, 0);
 			const auto unit = ConstantGenerator(module).getPrimitiveInt(typeName, 1);
 			
-			if (methodName == "__move_to") {
+			if (methodID == METHOD_ALIGNMASK) {
+				return genAlignMask(function, type);
+			} else if (methodID == METHOD_SIZEOF) {
+				return genSizeOf(function, type);
+			} else if (methodName == "__move_to") {
 				const auto moveToPtr = args[1].resolve(function);
 				const auto moveToPosition = args[2].resolve(function);
 				
