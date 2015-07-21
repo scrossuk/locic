2216829dc8e3b289748a3d17fd163dbd651063de
Stephen Cross <scross@scross.co.uk>
2015-07-21 19:12:49 +0100

    ref T30: Added __alignmask() and __sizeof() for typename_t primitive.

--- lib/CodeGen/Primitives/Typename.cpp
+++ lib/CodeGen/Primitives/Typename.cpp
@@ -1,6 +1,7 @@
 #include <locic/CodeGen/Function.hpp>
 #include <locic/CodeGen/InternalContext.hpp>
 #include <locic/CodeGen/Module.hpp>
+#include <locic/CodeGen/SizeOf.hpp>
 #include <locic/Support/MethodID.hpp>
 
 namespace locic {
@@ -8,7 +9,7 @@ namespace locic {
 	namespace CodeGen {
 		
 		llvm::Value* genTypenamePrimitiveMethodCall(Function& functionGenerator,
-		                                            const SEM::Type* /*type*/,
+		                                            const SEM::Type* type,
 		                                            const String& methodName,
 		                                            SEM::FunctionType /*functionType*/,
 		                                            PendingResultArray args) {
@@ -17,6 +18,10 @@ namespace locic {
 			const auto methodID = module.context().getMethodID(CanonicalizeMethodName(methodName));
 			
 			switch (methodID) {
+				case METHOD_ALIGNMASK:
+					return genAlignMask(functionGenerator, type);
+				case METHOD_SIZEOF:
+					return genSizeOf(functionGenerator, type);
 				case METHOD_IMPLICITCOPY:
 				case METHOD_COPY:
 					return args[0].resolveWithoutBind(functionGenerator);
