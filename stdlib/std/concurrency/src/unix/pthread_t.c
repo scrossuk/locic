0fb65bbd818202d6f551c941e5b4225f40bc3bad
Stephen Cross <scross@scross.co.uk>
2015-07-22 22:17:02 +0100

    ref T30: Added C implementation for pthread_t wrapper class.

---
+++ stdlib/std/concurrency/src/unix/pthread_t.c
@@ -0,0 +1,49 @@
+#include <pthread.h>
+#include <stdalign.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+size_t MT1N9pthread_tF1N11__alignmask() {
+	return alignof(pthread_t) - 1;
+}
+
+size_t MT1N9pthread_tF1N8__sizeof() {
+	return sizeof(pthread_t);
+}
+
+void MT1N9pthread_tF1N8__moveto(pthread_t* thread,
+                                uint8_t* dest,
+                                size_t offset) {
+	memcpy((dest + offset), thread, sizeof(*thread));
+}
+
+void MT1N9pthread_tF1N4null(pthread_t* thread) {
+	// Don't do anything until create().
+	(void) thread;
+	return;
+}
+
+void MT1N9pthread_tF1N9__destroy(pthread_t* thread) {
+	// Should already have joined...
+	(void) thread;
+}
+
+void MT1N9pthread_tF1N9__setdead(pthread_t* thread) {
+	// Nothing to do.
+	(void) thread;
+}
+
+int MT1N9pthread_tF1N6create(pthread_t* thread,
+                             void* attr,
+                             void* (*start_routine)(void*),
+                             void* arg) {
+	return pthread_create(thread,
+	                      attr,
+	                      start_routine,
+	                      arg);
+}
+
+int MT1N9pthread_tF1N4join(pthread_t* thread, void** retval) {
+	return pthread_join(*thread, retval);
+}
