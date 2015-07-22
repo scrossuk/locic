c1809094f213573caffb2eba63cb108e2acad509
Stephen Cross <scross@scross.co.uk>
2015-07-22 22:16:39 +0100

    ref T30: Added C implementation for pthread_mutex_t wrapper class.

---
+++ stdlib/std/concurrency/src/unix/pthread_mutex_t.c
@@ -0,0 +1,51 @@
+#include <pthread.h>
+#include <stdalign.h>
+#include <stddef.h>
+#include <stdint.h>
+#include <string.h>
+
+size_t MT1N15pthread_mutex_tF1N11__alignmask() {
+	return alignof(pthread_mutex_t) - 1;
+}
+
+size_t MT1N15pthread_mutex_tF1N8__sizeof() {
+	return sizeof(pthread_mutex_t);
+}
+
+void MT1N15pthread_mutex_tF1N8__moveto(pthread_mutex_t* mutex,
+                                        uint8_t* dest,
+                                        size_t offset) {
+	memcpy((dest + offset), mutex, sizeof(*mutex));
+}
+
+void MT1N15pthread_mutex_tF1N4null(pthread_mutex_t* mutex) {
+	// Don't do anything until init().
+	(void) mutex;
+	return;
+}
+
+void MT1N15pthread_mutex_tF1N9__destroy(pthread_mutex_t* mutex) {
+	// Should already have called destroy().
+	(void) mutex;
+}
+
+void MT1N15pthread_mutex_tF1N9__setdead(pthread_mutex_t* mutex) {
+	// Nothing to do.
+	(void) mutex;
+}
+
+int MT1N15pthread_mutex_tF1N4init(pthread_mutex_t* mutex, void* ptr) {
+	return pthread_mutex_init(mutex, ptr);
+}
+
+int MT1N15pthread_mutex_tF1N7destroy(pthread_mutex_t* mutex) {
+	return pthread_mutex_destroy(mutex);
+}
+
+int MT1N15pthread_mutex_tF1N4lock(pthread_mutex_t* mutex) {
+	return pthread_mutex_lock(mutex);
+}
+
+int MT1N15pthread_mutex_tF1N6unlock(pthread_mutex_t* mutex) {
+	return pthread_mutex_unlock(mutex);
+}
