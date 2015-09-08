41b270ae868ec49448249469bd3d3ac59283f85c
Stephen Cross <scross@scross.co.uk>
2015-09-08 21:05:38 +0100

    ref T5: Added initial implementation of static array methods to CodeGen.

---
+++ lib/CodeGen/Primitives/StaticArray.cpp
@@ -0,0 +1,101 @@
+#include <locic/CodeGen/ConstantGenerator.hpp>
+#include <locic/CodeGen/Function.hpp>
+#include <locic/CodeGen/GenType.hpp>
+#include <locic/CodeGen/GenValue.hpp>
+#include <locic/CodeGen/Interface.hpp>
+#include <locic/CodeGen/InternalContext.hpp>
+#include <locic/CodeGen/Module.hpp>
+#include <locic/CodeGen/SizeOf.hpp>
+#include <locic/CodeGen/Support.hpp>
+
+#include <locic/SEM/TemplateVar.hpp>
+#include <locic/SEM/Type.hpp>
+
+#include <locic/Support/MethodID.hpp>
+
+namespace locic {
+	
+	namespace CodeGen {
+		
+		llvm::Value* genStaticArrayPrimitiveMethodCall(Function& function,
+		                                               const SEM::Type* const type,
+		                                               const String& methodName,
+		                                               PendingResultArray args,
+		                                               llvm::Value* const /*hintResultValue*/) {
+			auto& builder = function.getBuilder();
+			auto& module = function.module();
+			
+			const auto methodID = module.context().getMethodID(CanonicalizeMethodName(methodName));
+			
+			const auto elementType = type->templateArguments().front().typeRefType();
+			const auto& elementCount = type->templateArguments().back();
+			
+			switch (methodID) {
+				case METHOD_ALIGNMASK: {
+					// Alignment of array is the same as alignment
+					// of each element.
+					return genAlignMask(function, elementType);
+				}
+				case METHOD_SIZEOF: {
+					return builder.CreateMul(genSizeOf(function, elementType),
+					                         genValue(function, elementCount));
+				}
+				case METHOD_UNINITIALIZED: {
+					// TODO: set elements to dead state.
+					return ConstantGenerator(module).getUndef(genType(module, type));
+				}
+				case METHOD_COPY:
+				case METHOD_IMPLICITCOPY: {
+					// TODO: need to call implicit copy methods!
+					return args[0].resolveWithoutBind(function);
+				}
+				case METHOD_ISVALID: {
+					// TODO!
+					(void) args[0].resolve(function);
+					return ConstantGenerator(module).getI1(true);
+				}
+				case METHOD_SETINVALID: {
+					// TODO!
+					(void) args[0].resolve(function);
+					return ConstantGenerator(module).getVoidUndef();
+				}
+				case METHOD_ISLIVE: {
+					// TODO!
+					(void) args[0].resolve(function);
+					return ConstantGenerator(module).getI1(true);
+				}
+				case METHOD_SETDEAD: {
+					// TODO!
+					(void) args[0].resolve(function);
+					return ConstantGenerator(module).getVoidUndef();
+				}
+				case METHOD_MOVETO: {
+					// TODO!
+					const auto methodOwner = args[0].resolveWithoutBind(function);
+					const auto moveToPtr = args[1].resolve(function);
+					const auto moveToPosition = args[2].resolve(function);
+					const auto destPtr = builder.CreateInBoundsGEP(moveToPtr, moveToPosition);
+					const auto llvmType = genType(module, type);
+					const auto castedDestPtr = builder.CreatePointerCast(destPtr, llvmType->getPointerTo());
+					builder.CreateStore(methodOwner, castedDestPtr);
+					return ConstantGenerator(module).getVoidUndef();
+				}
+				case METHOD_INDEX: {
+					const auto methodOwner = args[0].resolve(function);
+					const auto operand = args[1].resolve(function);
+					llvm::Value* const indexArray[] = {
+							ConstantGenerator(module).getSizeTValue(0),
+							operand
+						};
+					return builder.CreateInBoundsGEP(methodOwner, indexArray);
+				}
+				default:
+					printf("%s\n", methodName.c_str());
+					llvm_unreachable("Unknown static_array_t primitive method.");
+			}
+		}
+		
+	}
+	
+}
+
