29a723f66673d72d5aebfd7b3b8d2e134bc12fbd
Stephen Cross <scross@scross.co.uk>
2015-09-12 23:05:45 +0100

    ref T40: Refactored float primitive method generation to use MethodID switch.

--- lib/CodeGen/Primitives/Float.cpp
+++ lib/CodeGen/Primitives/Float.cpp
@@ -56,98 +56,127 @@ namespace locic {
 			
 			const auto methodOwner = methodID.isConstructor() ? nullptr : args[0].resolveWithoutBind(function);
 			
-			if (methodID == METHOD_ALIGNMASK) {
-				return ConstantGenerator(module).getSizeTValue(module.abi().typeAlign(genABIType(module, type)) - 1);
-			} else if (methodID == METHOD_SIZEOF) {
-				return ConstantGenerator(module).getSizeTValue(module.abi().typeSize(genABIType(module, type)));
-			} else if (methodID == METHOD_IMPLICITCOPY || methodID == METHOD_COPY) {
-				return methodOwner;
-			} else if (methodName == "__move_to") {
-				const auto moveToPtr = args[1].resolve(function);
-				const auto moveToPosition = args[2].resolve(function);
-				
-				const auto destPtr = builder.CreateInBoundsGEP(moveToPtr, moveToPosition);
-				const auto castedDestPtr = builder.CreatePointerCast(destPtr, genPointerType(module, type));
-				
-				genMoveStore(function, methodOwner, castedDestPtr, type);
-				return ConstantGenerator(module).getVoidUndef();
-			} else if (methodName == "create") {
-				return ConstantGenerator(module).getPrimitiveFloat(typeName, 0.0);
-			} else if (methodName == "__setdead" || methodName == "__set_dead") {
-				// Do nothing.
-				return ConstantGenerator(module).getVoidUndef();
-			} else if (methodName == "__islive" || methodName == "__is_live") {
-				return ConstantGenerator(module).getI1(true);
-			} else if (methodName.starts_with("implicit_cast_") || methodName.starts_with("cast_")) {
-				const auto argType = functionType.parameterTypes().front();
-				const auto operand = args[0].resolve(function);
-				const auto selfType = genType(module, type);
-				if (isFloatType(module, argType)) {
-					if (methodName.starts_with("implicit_cast_")) {
-						return builder.CreateFPExt(operand, selfType);
-					} else {
-						return builder.CreateFPTrunc(operand, selfType);
-					}
-				} else if (isUnsignedIntegerType(module, argType)) {
-					return builder.CreateUIToFP(operand, selfType);
-				} else if (isSignedIntegerType(module, argType)) {
-					return builder.CreateSIToFP(operand, selfType);
-				} else {
-					llvm_unreachable("Unknown float cast source type.");
-				}
-			} else if (isUnaryOp(methodName)) {
-				const auto zero = ConstantGenerator(module).getPrimitiveFloat(typeName, 0.0);
-				
-				if (methodName == "implicit_cast" || methodName == "cast") {
-					return callCastMethod(function, methodOwner, type, methodName, templateArgs.front().typeRefType(), hintResultValue);
-				} else if (methodName == "plus") {
+			switch (methodID) {
+				case METHOD_ALIGNMASK:
+					return ConstantGenerator(module).getSizeTValue(module.abi().typeAlign(genABIType(module, type)) - 1);
+				case METHOD_SIZEOF:
+					return ConstantGenerator(module).getSizeTValue(module.abi().typeSize(genABIType(module, type)));
+				case METHOD_IMPLICITCOPY:
+				case METHOD_COPY:
 					return methodOwner;
-				} else if (methodName == "minus") {
+				case METHOD_MOVETO: {
+					const auto moveToPtr = args[1].resolve(function);
+					const auto moveToPosition = args[2].resolve(function);
+					
+					const auto destPtr = builder.CreateInBoundsGEP(moveToPtr, moveToPosition);
+					const auto castedDestPtr = builder.CreatePointerCast(destPtr, genPointerType(module, type));
+					
+					genMoveStore(function, methodOwner, castedDestPtr, type);
+					return ConstantGenerator(module).getVoidUndef();
+				}
+				case METHOD_CREATE:
+					return ConstantGenerator(module).getPrimitiveFloat(typeName, 0.0);
+				case METHOD_SETDEAD:
+					// Do nothing.
+					return ConstantGenerator(module).getVoidUndef();
+				case METHOD_ISLIVE:
+					return ConstantGenerator(module).getI1(true);
+				case METHOD_IMPLICITCASTFROM:
+				case METHOD_CASTFROM: {
+					const auto argType = functionType.parameterTypes().front();
+					const auto operand = args[0].resolve(function);
+					const auto selfType = genType(module, type);
+					if (isFloatType(module, argType)) {
+						if (methodName.starts_with("implicit_cast_")) {
+							return builder.CreateFPExt(operand, selfType);
+						} else {
+							return builder.CreateFPTrunc(operand, selfType);
+						}
+					} else if (isUnsignedIntegerType(module, argType)) {
+						return builder.CreateUIToFP(operand, selfType);
+					} else if (isSignedIntegerType(module, argType)) {
+						return builder.CreateSIToFP(operand, selfType);
+					} else {
+						llvm_unreachable("Unknown float cast source type.");
+					}
+				}
+				case METHOD_IMPLICITCAST:
+				case METHOD_CAST:
+					return callCastMethod(function, methodOwner, type, methodName, templateArgs.front().typeRefType(), hintResultValue);
+				case METHOD_PLUS:
+					return methodOwner;
+				case METHOD_MINUS:
 					return builder.CreateFNeg(methodOwner);
-				} else if (methodName == "isZero") {
+				case METHOD_ISZERO: {
+					const auto zero = ConstantGenerator(module).getPrimitiveFloat(typeName, 0.0);
 					return builder.CreateFCmpOEQ(methodOwner, zero);
-				} else if (methodName == "isPositive") {
+				}
+				case METHOD_ISPOSITIVE: {
+					const auto zero = ConstantGenerator(module).getPrimitiveFloat(typeName, 0.0);
 					return builder.CreateFCmpOGT(methodOwner, zero);
-				} else if (methodName == "isNegative") {
+				}
+				case METHOD_ISNEGATIVE: {
+					const auto zero = ConstantGenerator(module).getPrimitiveFloat(typeName, 0.0);
 					return builder.CreateFCmpOLT(methodOwner, zero);
-				} else if (methodName == "abs") {
+				}
+				case METHOD_ABS: {
 					// Generates: (value < 0) ? -value : value.
+					const auto zero = ConstantGenerator(module).getPrimitiveFloat(typeName, 0.0);
 					const auto lessThanZero = builder.CreateFCmpOLT(methodOwner, zero);
 					return builder.CreateSelect(lessThanZero, builder.CreateFNeg(methodOwner), methodOwner);
-				} else if (methodName == "sqrt") {
+				}
+				case METHOD_SQRT: {
 					llvm::Type* const intrinsicTypes[] = { methodOwner->getType() };
 					const auto sqrtIntrinsic = llvm::Intrinsic::getDeclaration(module.getLLVMModulePtr(), llvm::Intrinsic::sqrt, intrinsicTypes);
 					llvm::Value* const sqrtArgs[] = { methodOwner };
 					return builder.CreateCall(sqrtIntrinsic, sqrtArgs);
-				} else {
-					llvm_unreachable("Unknown primitive unary op.");
 				}
-			} else if (isBinaryOp(methodName)) {
-				const auto operand = args[1].resolveWithoutBind(function);
-				
-				if (methodName == "add") {
+				case METHOD_ADD: {
+					const auto operand = args[1].resolveWithoutBind(function);
 					return builder.CreateFAdd(methodOwner, operand);
-				} else if (methodName == "subtract") {
+				}
+				case METHOD_SUBTRACT: {
+					const auto operand = args[1].resolveWithoutBind(function);
 					return builder.CreateFSub(methodOwner, operand);
-				} else if (methodName == "multiply") {
+				}
+				case METHOD_MULTIPLY: {
+					const auto operand = args[1].resolveWithoutBind(function);
 					return builder.CreateFMul(methodOwner, operand);
-				} else if (methodName == "divide") {
+				}
+				case METHOD_DIVIDE: {
+					const auto operand = args[1].resolveWithoutBind(function);
 					return builder.CreateFDiv(methodOwner, operand);
-				} else if (methodName == "modulo") {
+				}
+				case METHOD_MODULO: {
+					const auto operand = args[1].resolveWithoutBind(function);
 					return builder.CreateFRem(methodOwner, operand);
-				} else if (methodName == "equal") {
+				}
+				case METHOD_EQUAL: {
+					const auto operand = args[1].resolveWithoutBind(function);
 					return builder.CreateFCmpOEQ(methodOwner, operand);
-				} else if (methodName == "not_equal") {
+				}
+				case METHOD_NOTEQUAL: {
+					const auto operand = args[1].resolveWithoutBind(function);
 					return builder.CreateFCmpONE(methodOwner, operand);
-				} else if (methodName == "less_than") {
+				}
+				case METHOD_LESSTHAN: {
+					const auto operand = args[1].resolveWithoutBind(function);
 					return builder.CreateFCmpOLT(methodOwner, operand);
-				} else if (methodName == "less_than_or_equal") {
+				}
+				case METHOD_LESSTHANOREQUAL: {
+					const auto operand = args[1].resolveWithoutBind(function);
 					return builder.CreateFCmpOLE(methodOwner, operand);
-				} else if (methodName == "greater_than") {
+				}
+				case METHOD_GREATERTHAN: {
+					const auto operand = args[1].resolveWithoutBind(function);
 					return builder.CreateFCmpOGT(methodOwner, operand);
-				} else if (methodName == "greater_than_or_equal") {
+				}
+				case METHOD_GREATERTHANOREQUAL: {
+					const auto operand = args[1].resolveWithoutBind(function);
 					return builder.CreateFCmpOGE(methodOwner, operand);
-				} else if (methodName == "compare") {
+				}
+				case METHOD_COMPARE: {
+					const auto operand = args[1].resolveWithoutBind(function);
 					const auto isLessThan = builder.CreateFCmpOLT(methodOwner, operand);
 					const auto isGreaterThan = builder.CreateFCmpOGT(methodOwner, operand);
 					const auto minusOne = ConstantGenerator(module).getI8(-1);
@@ -155,12 +184,10 @@ namespace locic {
 					const auto plusOne = ConstantGenerator(module).getI8(1);
 					return builder.CreateSelect(isLessThan, minusOne,
 							builder.CreateSelect(isGreaterThan, plusOne, zero));
-				} else {
-					llvm_unreachable("Unknown primitive binary op.");
 				}
-			} else {
-				printf("%s\n", methodName.c_str());
-				llvm_unreachable("Unknown primitive method.");
+				default:
+					printf("%s\n", methodName.c_str());
+					llvm_unreachable("Unknown primitive method.");
 			}
 		}
 		
