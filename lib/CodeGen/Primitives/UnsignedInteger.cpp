eed38bcc84a09607d7fa14e0da9b2afabe832ed0
Stephen Cross <scross@scross.co.uk>
2015-09-12 01:40:11 +0100

    ref T5: Fixed unsigned integer alignmask/sizeof generation to directly compute result.

--- lib/CodeGen/Primitives/UnsignedInteger.cpp
+++ lib/CodeGen/Primitives/UnsignedInteger.cpp
@@ -121,10 +121,10 @@ namespace locic {
 					}
 				}
 				case METHOD_ALIGNMASK: {
-					return genAlignMask(function, type);
+					return ConstantGenerator(module).getSizeTValue(module.abi().typeAlign(genABIType(module, type)) - 1);
 				}
 				case METHOD_SIZEOF: {
-					return genSizeOf(function, type);
+					return ConstantGenerator(module).getSizeTValue(module.abi().typeSize(genABIType(module, type)));
 				}
 					
 				case METHOD_IMPLICITCAST:
