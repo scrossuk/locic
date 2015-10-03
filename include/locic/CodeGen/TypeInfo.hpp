fd449556d74cde10c0355c94e1aeb5bdbee34d11
Stephen Cross <scross@scross.co.uk>
2015-10-03 15:26:42 +0100

    ref T40: Added some API comments to TypeInfo.

--- include/locic/CodeGen/TypeInfo.hpp
+++ include/locic/CodeGen/TypeInfo.hpp
@@ -30,14 +30,51 @@ namespace locic {
 			
 			bool objectHasCustomDestructor(const SEM::TypeInstance& typeInstance) const;
 			
+			/**
+			 * \brief Query whether a type has a custom move operation.
+			 * 
+			 * This will determine whether the type has a custom
+			 * move operation (i.e. we can't just issue a memcpy).
+			 * 
+			 * For class declarations this always returns true, since
+			 * we must assume they have a custom move method. For
+			 * known types they will have a custom move method if
+			 * one of their child types has a custom move method,
+			 * so this will also return true in that case.
+			 */
 			bool hasCustomMove(const SEM::Type* type) const;
 			
+			/**
+			 * \brief Query whether a type instance has a custom move operation.
+			 * 
+			 * This will determine whether the type has a custom
+			 * move method.
+			 * 
+			 * For class declarations this always returns true, since
+			 * we must assume they have a custom move method. For
+			 * known types they will have a custom move method if
+			 * one of their child types has a custom move method, so
+			 * this will also return true in that case.
+			 */
 			bool objectHasCustomMove(const SEM::TypeInstance& typeInstance) const;
 			
+			/**
+			 * \brief Query whether a type instance has a custom move method.
+			 * 
+			 * This will determine whether the type has a custom move method;
+			 * it doesn't check whether any of the member values have custom
+			 * move methods.
+			 */
 			bool objectHasCustomMoveMethod(const SEM::TypeInstance& typeInstance) const;
 			
+			/**
+			 * \brief Query whether a type has a liveness indicator.
+			 */
 			bool hasLivenessIndicator(const SEM::Type* type) const;
 			
+			/**
+			 * \brief Query whether a type instance has a liveness indicator.
+			 */
 			bool objectHasLivenessIndicator(const SEM::TypeInstance& typeInstance) const;
 			
 		private:
