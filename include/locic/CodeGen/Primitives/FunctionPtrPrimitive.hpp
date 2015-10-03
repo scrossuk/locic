551546848c8c6f34b7ca7d3ddfb641c730839c22
Stephen Cross <scross@scross.co.uk>
2015-10-03 17:48:52 +0100

    ref T40: Added declarations for new Primitive implementations.

---
+++ include/locic/CodeGen/Primitives/FunctionPtrPrimitive.hpp
@@ -0,0 +1,62 @@
+#ifndef LOCIC_CODEGEN_PRIMITIVES_FUNCTIONPTRPRIMITIVE_HPP
+#define LOCIC_CODEGEN_PRIMITIVES_FUNCTIONPTRPRIMITIVE_HPP
+
+#include <llvm-abi/Type.hpp>
+
+#include <locic/CodeGen/MethodInfo.hpp>
+#include <locic/CodeGen/PendingResult.hpp>
+
+namespace locic {
+	
+	class PrimitiveID;
+	class String;
+	
+	namespace SEM {
+		
+		class Function;
+		class Type;
+		class TypeInstance;
+		
+	}
+	
+	namespace CodeGen {
+		
+		class FunctionPtrPrimitive: public Primitive {
+		public:
+			FunctionPtrPrimitive(const SEM::TypeInstance& typeInstance);
+			
+			bool isSizeAlwaysKnown(const TypeInfo& typeInfo,
+			                       llvm::ArrayRef<SEM::Value> templateVariables) const;
+			
+			bool isSizeKnownInThisModule(const TypeInfo& typeInfo,
+			                             llvm::ArrayRef<SEM::Value> templateVariables) const;
+			
+			bool hasCustomDestructor(const TypeInfo& typeInfo,
+			                         llvm::ArrayRef<SEM::Value> templateVariables) const;
+			
+			bool hasCustomMove(const TypeInfo& typeInfo,
+			                   llvm::ArrayRef<SEM::Value> templateVariables) const;
+			
+			llvm_abi::Type* getABIType(Module& module,
+			                           llvm_abi::Context& context,
+			                           llvm::ArrayRef<SEM::Value> templateVariables) const;
+			
+			llvm::Type* getIRType(Module& module,
+			                      const TypeGenerator& typeGenerator,
+			                      llvm::ArrayRef<SEM::Value> templateVariables) const;
+			
+			llvm::Value* emitMethod(IREmitter& irEmitter, MethodID methodID,
+			                        llvm::ArrayRef<SEM::Value> typeTemplateVariables,
+			                        llvm::ArrayRef<SEM::Value> functionTemplateVariables,
+			                        PendingResultArray args) const;
+			
+		private:
+			const SEM::TypeInstance& typeInstance_;
+			
+		};
+		
+	}
+	
+}
+
+#endif
