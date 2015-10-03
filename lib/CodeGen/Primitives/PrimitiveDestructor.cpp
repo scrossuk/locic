022dd4451a034a1558cb47d91f9716419d089a7a
Stephen Cross <scross@scross.co.uk>
2015-10-03 17:30:19 +0100

    ref T40: Modified genPrimitiveDestructorCall() to use new Primitive interface.

--- lib/CodeGen/Primitives/PrimitiveDestructor.cpp
+++ lib/CodeGen/Primitives/PrimitiveDestructor.cpp
@@ -2,38 +2,37 @@
 
 #include <string>
 
-#include <locic/CodeGen/ArgInfo.hpp>
 #include <locic/CodeGen/Function.hpp>
-#include <locic/CodeGen/GenType.hpp>
+#include <locic/CodeGen/IREmitter.hpp>
 #include <locic/CodeGen/Module.hpp>
+#include <locic/CodeGen/Primitive.hpp>
 #include <locic/CodeGen/Primitives.hpp>
-#include <locic/CodeGen/SizeOf.hpp>
-#include <locic/CodeGen/TypeGenerator.hpp>
-#include <locic/CodeGen/TypeInfo.hpp>
-#include <locic/CodeGen/UnwindAction.hpp>
+#include <locic/CodeGen/Support.hpp>
+#include <locic/Support/MethodID.hpp>
 
 namespace locic {
-
-	namespace CodeGen {
 	
+	namespace CodeGen {
+		
 		void genPrimitiveDestructorCall(Function& function, const SEM::Type* const type, llvm::Value* value) {
 			assert(value->getType()->isPointerTy());
+			assert(type->isPrimitive());
 			
 			auto& module = function.module();
-			
-			assert(type->isPrimitive());
-			const auto canonicalMethodName = module.getCString("__destroy");
-			const auto functionType = type->getObjectType()->functions().at(canonicalMethodName)->type();
-			
-			const auto methodName = module.getCString("__destroy");
-			MethodInfo methodInfo(type, methodName, functionType, {});
 			
 			PendingResultArray arguments;
 			
 			const RefPendingResult contextPendingResult(value, type);
 			arguments.push_back(contextPendingResult);
 			
-			(void) genTrivialPrimitiveFunctionCall(function, std::move(methodInfo), std::move(arguments));
+			IREmitter irEmitter(function);
+			
+			const auto& primitive = module.getPrimitive(*(type->getObjectType()));
+			(void) primitive.emitMethod(irEmitter,
+			                            METHOD_DESTROY,
+			                            arrayRef(type->templateArguments()),
+			                            /*functionTemplateArguments=*/llvm::ArrayRef<SEM::Value>(),
+			                            std::move(arguments));
 		}
 		
 	}
