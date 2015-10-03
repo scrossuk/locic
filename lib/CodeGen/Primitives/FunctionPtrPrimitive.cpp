ee913ddecb379d84fab01213d5b7c7c4eddee33a
Stephen Cross <scross@scross.co.uk>
2015-10-03 17:37:51 +0100

    ref T40: Added new FunctionPtrPrimitive implementation of Primitive interface.

---
+++ lib/CodeGen/Primitives/FunctionPtrPrimitive.cpp
@@ -0,0 +1,272 @@
+#include <assert.h>
+
+#include <stdexcept>
+#include <string>
+#include <vector>
+
+#include <locic/CodeGen/ArgInfo.hpp>
+#include <locic/CodeGen/ConstantGenerator.hpp>
+#include <locic/CodeGen/Debug.hpp>
+#include <locic/CodeGen/Destructor.hpp>
+#include <locic/CodeGen/Function.hpp>
+#include <locic/CodeGen/FunctionCallInfo.hpp>
+#include <locic/CodeGen/GenABIType.hpp>
+#include <locic/CodeGen/GenFunctionCall.hpp>
+#include <locic/CodeGen/GenType.hpp>
+#include <locic/CodeGen/GenVTable.hpp>
+#include <locic/CodeGen/Interface.hpp>
+#include <locic/CodeGen/InternalContext.hpp>
+#include <locic/CodeGen/IREmitter.hpp>
+#include <locic/CodeGen/Liveness.hpp>
+#include <locic/CodeGen/Memory.hpp>
+#include <locic/CodeGen/Module.hpp>
+#include <locic/CodeGen/Move.hpp>
+#include <locic/CodeGen/Primitive.hpp>
+#include <locic/CodeGen/Primitives.hpp>
+#include <locic/CodeGen/Primitives/FunctionPtrPrimitive.hpp>
+#include <locic/CodeGen/Routines.hpp>
+#include <locic/CodeGen/SizeOf.hpp>
+#include <locic/CodeGen/Support.hpp>
+#include <locic/CodeGen/Template.hpp>
+#include <locic/CodeGen/TypeGenerator.hpp>
+#include <locic/CodeGen/UnwindAction.hpp>
+#include <locic/CodeGen/VTable.hpp>
+
+#include <locic/Support/MethodID.hpp>
+
+namespace locic {
+	
+	namespace CodeGen {
+		
+		FunctionPtrPrimitive::FunctionPtrPrimitive(const SEM::TypeInstance& typeInstance)
+		: typeInstance_(typeInstance) { }
+		
+		bool FunctionPtrPrimitive::isSizeAlwaysKnown(const TypeInfo& /*typeInfo*/,
+		                                             llvm::ArrayRef<SEM::Value> /*templateArguments*/) const {
+			return true;
+		}
+		
+		bool FunctionPtrPrimitive::isSizeKnownInThisModule(const TypeInfo& /*typeInfo*/,
+		                                                   llvm::ArrayRef<SEM::Value> /*templateArguments*/) const {
+			return true;
+		}
+		
+		bool FunctionPtrPrimitive::hasCustomDestructor(const TypeInfo& /*typeInfo*/,
+		                                               llvm::ArrayRef<SEM::Value> /*templateArguments*/) const {
+			return false;
+		}
+		
+		bool FunctionPtrPrimitive::hasCustomMove(const TypeInfo& /*typeInfo*/,
+		                                         llvm::ArrayRef<SEM::Value> /*templateArguments*/) const {
+			return false;
+		}
+		
+		llvm_abi::Type* FunctionPtrPrimitive::getABIType(Module& module,
+		                                                 llvm_abi::Context& abiContext,
+		                                                 llvm::ArrayRef<SEM::Value> /*templateArguments*/) const {
+			switch (typeInstance_.primitiveID()) {
+				case PrimitiveFunctionPtr:
+				case PrimitiveMethodFunctionPtr:
+				case PrimitiveVarArgFunctionPtr:
+					return llvm_abi::Type::Pointer(abiContext);
+				case PrimitiveTemplatedFunctionPtr:
+				case PrimitiveTemplatedMethodFunctionPtr: {
+					std::vector<llvm_abi::Type*> types;
+					types.reserve(2);
+					types.push_back(llvm_abi::Type::Pointer(abiContext));
+					types.push_back(templateGeneratorType(module).first);
+					return llvm_abi::Type::AutoStruct(abiContext, types);
+				}
+				case PrimitiveMethod: {
+					std::vector<llvm_abi::Type*> types;
+					types.reserve(2);
+					types.push_back(llvm_abi::Type::Pointer(abiContext));
+					types.push_back(getBasicPrimitiveABIType(module, PrimitiveMethodFunctionPtr));
+					return llvm_abi::Type::AutoStruct(abiContext, types);
+				}
+				case PrimitiveTemplatedMethod: {
+					std::vector<llvm_abi::Type*> types;
+					types.reserve(2);
+					types.push_back(llvm_abi::Type::Pointer(abiContext));
+					types.push_back(getBasicPrimitiveABIType(module, PrimitiveTemplatedMethodFunctionPtr));
+					return llvm_abi::Type::AutoStruct(abiContext, types);
+				}
+				case PrimitiveInterfaceMethod: {
+					return interfaceMethodType(module).first;
+				}
+				case PrimitiveStaticInterfaceMethod: {
+					return staticInterfaceMethodType(module).first;
+				}
+				default:
+					llvm_unreachable("Invalid functionptr primitive ID.");
+			}
+		}
+		
+		llvm::Type* FunctionPtrPrimitive::getIRType(Module& module,
+		                                            const TypeGenerator& typeGenerator,
+		                                            llvm::ArrayRef<SEM::Value> /*templateArguments*/) const {
+			switch (typeInstance_.primitiveID()) {
+				case PrimitiveFunctionPtr:
+				case PrimitiveMethodFunctionPtr:
+				case PrimitiveVarArgFunctionPtr:
+				case PrimitivePtrLval:
+					return typeGenerator.getI8PtrType();
+				case PrimitiveTemplatedFunctionPtr:
+				case PrimitiveTemplatedMethodFunctionPtr: {
+					llvm::Type* const memberTypes[] = {
+						typeGenerator.getI8PtrType(),
+						templateGeneratorType(module).second
+					};
+					return typeGenerator.getStructType(memberTypes);
+				}
+				case PrimitiveMethod: {
+					llvm::Type* const memberTypes[] = {
+						typeGenerator.getI8PtrType(),
+						getBasicPrimitiveType(module, PrimitiveMethodFunctionPtr)
+					};
+					return typeGenerator.getStructType(memberTypes);
+				}
+				case PrimitiveTemplatedMethod: {
+					llvm::Type* const memberTypes[] = {
+						typeGenerator.getI8PtrType(),
+						getBasicPrimitiveType(module, PrimitiveTemplatedMethodFunctionPtr)
+					};
+					return typeGenerator.getStructType(memberTypes);
+				}
+				case PrimitiveInterfaceMethod: {
+					return interfaceMethodType(module).second;
+				}
+				case PrimitiveStaticInterfaceMethod: {
+					return staticInterfaceMethodType(module).second;
+				}
+				default:
+					llvm_unreachable("Invalid functionptr primitive ID.");
+			}
+		}
+		
+		namespace {
+			
+			llvm::Value* genFunctionPtrNullMethod(Function& function, const SEM::Type* const type) {
+				auto& module = function.module();
+				
+				const auto llvmType = genType(module, type);
+				return ConstantGenerator(module).getNull(llvmType);
+			}
+			
+			llvm::Value* genFunctionPtrCopyMethod(Function& function, PendingResultArray args) {
+				return args[0].resolveWithoutBind(function);
+			}
+			
+			llvm::Value* genFunctionPtrCompareMethod(Function& function, PendingResultArray args) {
+				auto& builder = function.getBuilder();
+				auto& module = function.module();
+				
+				const auto methodOwner = args[0].resolveWithoutBind(function);
+				const auto operand = args[1].resolveWithoutBind(function);
+				
+				const auto minusOneResult = ConstantGenerator(module).getI8(-1);
+				const auto zeroResult = ConstantGenerator(module).getI8(0);
+				const auto plusOneResult = ConstantGenerator(module).getI8(1);
+				
+				const auto isLessThan = builder.CreateICmpULT(methodOwner, operand);
+				const auto isGreaterThan = builder.CreateICmpUGT(methodOwner, operand);
+				
+				return builder.CreateSelect(isLessThan, minusOneResult,
+					builder.CreateSelect(isGreaterThan, plusOneResult, zeroResult));
+			}
+			
+			llvm::Value* genFunctionPtrSetDeadMethod(Function& function) {
+				auto& module = function.module();
+				// Do nothing.
+				return ConstantGenerator(module).getVoidUndef();
+			}
+			
+			llvm::Value* genFunctionPtrIsLiveMethod(Function& function) {
+				auto& module = function.module();
+				return ConstantGenerator(module).getI1(true);
+			}
+			
+			llvm::Value* genFunctionPtrMoveToMethod(Function& function, const SEM::Type* const type, PendingResultArray args) {
+				auto& builder = function.getBuilder();
+				auto& module = function.module();
+				
+				const auto methodOwner = args[0].resolveWithoutBind(function);
+				
+				const auto moveToPtr = args[1].resolve(function);
+				const auto moveToPosition = args[2].resolve(function);
+				
+				const auto destPtr = builder.CreateInBoundsGEP(moveToPtr, moveToPosition);
+				const auto castedDestPtr = builder.CreatePointerCast(destPtr, genPointerType(module, type));
+				
+				genMoveStore(function, methodOwner, castedDestPtr, type);
+				return ConstantGenerator(module).getVoidUndef();
+			}
+			
+			llvm::Value* genFunctionPtrCallMethod(Function& function, const SEM::Type* type, PendingResultArray args, llvm::Value* hintResultValue) {
+				const auto functionValue = args[0].resolveWithoutBind(function);
+				
+				FunctionCallInfo callInfo;
+				if (type->isBuiltInTemplatedFunctionPtr()) {
+					callInfo.functionPtr = function.getBuilder().CreateExtractValue(functionValue, { 0 });
+					callInfo.templateGenerator = function.getBuilder().CreateExtractValue(functionValue, { 1 });
+				} else {
+					callInfo.functionPtr = functionValue;
+				}
+				
+				PendingResultArray callArgs;
+				for (size_t i = 1; i < args.size(); i++) {
+					callArgs.push_back(std::move(args[i]));
+				}
+				
+				return genFunctionCall(function, type->asFunctionType(), callInfo, std::move(callArgs), hintResultValue);
+			}
+			
+		}
+		
+		llvm::Value* FunctionPtrPrimitive::emitMethod(IREmitter& irEmitter,
+		                                              const MethodID methodID,
+		                                              llvm::ArrayRef<SEM::Value> typeTemplateArguments,
+		                                              llvm::ArrayRef<SEM::Value> /*functionTemplateArguments*/,
+		                                              PendingResultArray args) const {
+			auto& function = irEmitter.function();
+			auto& module = irEmitter.module();
+			
+			SEM::ValueArray valueArray;
+			for (const auto& value: typeTemplateArguments) {
+				valueArray.push_back(value.copy());
+			}
+			const auto type = SEM::Type::Object(&typeInstance_,
+			                                    std::move(valueArray));
+			
+			switch (methodID) {
+				case METHOD_NULL:
+					return genFunctionPtrNullMethod(function, type);
+				case METHOD_ALIGNMASK:
+					return ConstantGenerator(module).getSizeTValue(module.abi().typeAlign(genABIType(module, type)) - 1);
+				case METHOD_SIZEOF:
+					return ConstantGenerator(module).getSizeTValue(module.abi().typeSize(genABIType(module, type)));
+				case METHOD_COPY:
+				case METHOD_IMPLICITCOPY:
+					return genFunctionPtrCopyMethod(function, std::move(args));
+				case METHOD_COMPARE:
+					return genFunctionPtrCompareMethod(function, std::move(args));
+				case METHOD_SETDEAD:
+					return genFunctionPtrSetDeadMethod(function);
+				case METHOD_MOVETO:
+					return genFunctionPtrMoveToMethod(function, type, std::move(args));
+				case METHOD_ISLIVE:
+					return genFunctionPtrIsLiveMethod(function);
+				case METHOD_CALL:
+					return genFunctionPtrCallMethod(function,
+					                                type,
+					                                std::move(args),
+					                                irEmitter.hintResultValue());
+				default:
+					llvm_unreachable("Unknown function_ptr primitive method.");
+			}
+		}
+		
+	}
+	
+}
+
