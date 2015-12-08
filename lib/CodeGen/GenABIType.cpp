3ef286d39ba8fe8c219a8996b8bbdebd1af28578
Stephen Cross <scross@scross.co.uk>
2015-12-08 00:46:15 +0000

    Added genABIObjectType() to reduce calls to TypeInstance::selfType().

--- lib/CodeGen/GenABIType.cpp
+++ lib/CodeGen/GenABIType.cpp
@@ -30,60 +30,71 @@ namespace locic {
 			}
 		}
 		
-		llvm_abi::Type genABIType(Module& module, const SEM::Type* type) {
+		llvm_abi::Type genABIObjectType(Module& module, const SEM::TypeInstance& typeInstance,
+		                                const SEM::ValueArray& templateArguments) {
 			auto& abiTypeBuilder = module.abiTypeBuilder();
 			
+			if (typeInstance.isPrimitive()) {
+				return getPrimitiveABIType(module, SEM::Type::Object(&typeInstance, templateArguments.copy()));
+			} else {
+				const auto mangledName = mangleObjectType(module, &typeInstance).asStdString();
+				
+				llvm::SmallVector<llvm_abi::Type, 8> members;
+				
+				if (typeInstance.isUnion()) {
+					members.reserve(typeInstance.variables().size() + 1);
+					
+					for (const auto var: typeInstance.variables()) {
+						members.push_back(genABIType(module, var->type()));
+					}
+					
+					return abiTypeBuilder.getUnionTy(members, mangledName);
+				} else if (typeInstance.isUnionDatatype()) {
+					members.reserve(typeInstance.variants().size());
+					
+					for (auto variantTypeInstance: typeInstance.variants()) {
+						members.push_back(genABIObjectType(module, *variantTypeInstance,
+						                                   variantTypeInstance->selfTemplateArgs()));
+					}
+					
+					const auto unionType = abiTypeBuilder.getUnionTy(members);
+					return abiTypeBuilder.getStructTy({llvm_abi::Int8Ty,
+					                                   unionType}, mangledName);
+				} else {
+					members.reserve(typeInstance.variables().size() + 1);
+					
+					for (const auto var: typeInstance.variables()) {
+						members.push_back(genABIType(module, var->type()));
+					}
+					
+					const auto livenessIndicator = getLivenessIndicator(module, typeInstance);
+					if (livenessIndicator.isSuffixByte()) {
+						// Add suffix byte.
+						members.push_back(llvm_abi::Int8Ty);
+					}
+					
+					// Class sizes must be at least one byte; empty structs
+					// are zero size for compatibility with the GCC extension.
+					if (!typeInstance.isStruct() && members.empty()) {
+						members.push_back(llvm_abi::Int8Ty);
+					}
+					
+					return abiTypeBuilder.getStructTy(members, mangledName);
+				}
+			}
+		}
+		
+		llvm_abi::Type genABIType(Module& module, const SEM::Type* type) {
 			switch (type->kind()) {
 				case SEM::Type::OBJECT: {
 					const auto typeInstance = type->getObjectType();
 					
 					if (typeInstance->isPrimitive()) {
 						return getPrimitiveABIType(module, type);
-					} else {
-						const auto mangledName = mangleObjectType(module, typeInstance).asStdString();
-						
-						llvm::SmallVector<llvm_abi::Type, 8> members;
-						
-						if (typeInstance->isUnion()) {
-							members.reserve(typeInstance->variables().size() + 1);
-							
-							for (const auto var: typeInstance->variables()) {
-								members.push_back(genABIType(module, var->type()));
-							}
-							
-							return abiTypeBuilder.getUnionTy(members, mangledName);
-						} else if (typeInstance->isUnionDatatype()) {
-							members.reserve(typeInstance->variants().size());
-							
-							for (auto variantTypeInstance: typeInstance->variants()) {
-								members.push_back(genABIType(module, variantTypeInstance->selfType()));
-							}
-							
-							const auto unionType = abiTypeBuilder.getUnionTy(members);
-							return abiTypeBuilder.getStructTy({llvm_abi::Int8Ty,
-							                                   unionType}, mangledName);
-						} else {
-							members.reserve(typeInstance->variables().size() + 1);
-							
-							for (const auto var: typeInstance->variables()) {
-								members.push_back(genABIType(module, var->type()));
-							}
-							
-							const auto livenessIndicator = getLivenessIndicator(module, *typeInstance);
-							if (livenessIndicator.isSuffixByte()) {
-								// Add suffix byte.
-								members.push_back(llvm_abi::Int8Ty);
-							}
-							
-							// Class sizes must be at least one byte; empty structs
-							// are zero size for compatibility with the GCC extension.
-							if (!typeInstance->isStruct() && members.empty()) {
-								members.push_back(llvm_abi::Int8Ty);
-							}
-							
-							return abiTypeBuilder.getStructTy(members, mangledName);
-						}
 					}
+					
+					return genABIObjectType(module, *typeInstance,
+					                        type->templateArguments());
 				}
 				case SEM::Type::ALIAS: {
 					return genABIType(module, type->resolveAliases());
