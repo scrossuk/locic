f8845617f0edb1b214e9c1af44eef8a97e98ab90
Stephen Cross <scross@scross.co.uk>
2015-12-27 18:55:03 +0000

    Added Parser::Token as alias of Lex::Token.

---
+++ include/locic/Parser/Token.hpp
@@ -0,0 +1,16 @@
+#ifndef LOCIC_PARSER_TOKEN_HPP
+#define LOCIC_PARSER_TOKEN_HPP
+
+#include <locic/Lex/Token.hpp>
+
+namespace locic {
+	
+	namespace Parser {
+		
+		using Token = Lex::Token;
+		
+	}
+	
+}
+
+#endif
