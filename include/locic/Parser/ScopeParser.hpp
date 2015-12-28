307edee368046ab806776f7b5ae984b94d0243f0
Stephen Cross <scross@scross.co.uk>
2015-12-28 18:22:12 +0000

    Added initial API for ScopeParser.

---
+++ include/locic/Parser/ScopeParser.hpp
@@ -0,0 +1,28 @@
+#ifndef LOCIC_PARSER_SCOPEPARSER_HPP
+#define LOCIC_PARSER_SCOPEPARSER_HPP
+
+#include <locic/AST.hpp>
+
+namespace locic {
+	
+	namespace Parser {
+		
+		class TokenReader;
+		
+		class ScopeParser {
+		public:
+			ScopeParser(TokenReader& reader);
+			~ScopeParser();
+			
+			AST::Node<AST::Scope> parseScope();
+			
+		private:
+			TokenReader& reader_;
+			
+		};
+		
+	}
+	
+}
+
+#endif
