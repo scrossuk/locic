2396a70b455f557f70e7130c707b8a2fe10cb805
Stephen Cross <scross@scross.co.uk>
2015-12-28 18:23:21 +0000

    Added API for StatementBuilder.

---
+++ include/locic/Parser/StatementParser.hpp
@@ -0,0 +1,55 @@
+#ifndef LOCIC_PARSER_STATEMENTPARSER_HPP
+#define LOCIC_PARSER_STATEMENTPARSER_HPP
+
+#include <locic/AST.hpp>
+#include <locic/Parser/StatementBuilder.hpp>
+#include <locic/Parser/Token.hpp>
+
+namespace locic {
+	
+	namespace Parser {
+		
+		class TokenReader;
+		
+		class StatementParser {
+		public:
+			StatementParser(TokenReader& reader);
+			~StatementParser();
+			
+			AST::Node<AST::Statement> parseStatement();
+			
+			AST::Node<AST::Statement> parseScopeStatement();
+			
+			AST::Node<AST::Statement> parseIfStatement();
+			
+			AST::Node<AST::IfClause> parseIfClause();
+			
+			AST::Node<AST::Statement> parseSwitchStatement();
+			
+			AST::Node<AST::SwitchCaseList> parseSwitchCaseList();
+			
+			AST::Node<AST::SwitchCase> parseSwitchCase();
+			
+			AST::Node<AST::DefaultCase> parseSwitchDefaultCase();
+			
+			AST::Node<AST::Statement> parseWhileStatement();
+			
+			AST::Node<AST::Statement> parseForStatement();
+			
+			bool isVarDeclStartToken(Token::Kind kind);
+			
+			AST::Node<AST::Statement> parseVarDeclStatement();
+			
+			AST::Node<AST::Statement> parseValueOrVarDeclStatement();
+			
+		private:
+			TokenReader& reader_;
+			StatementBuilder builder_;
+			
+		};
+		
+	}
+	
+}
+
+#endif
