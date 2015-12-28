c1af615aa74b8c074fff79eb766c23cf19986605
Stephen Cross <scross@scross.co.uk>
2015-12-28 13:11:20 +0000

    Added ValueBuilder class for simplifying construction of AST values.

---
+++ include/locic/Parser/ValueBuilder.hpp
@@ -0,0 +1,117 @@
+#ifndef LOCIC_PARSER_VALUEBUILDER_HPP
+#define LOCIC_PARSER_VALUEBUILDER_HPP
+
+#include <locic/AST.hpp>
+
+namespace locic {
+	
+	class Constant;
+	class String;
+	
+	namespace Debug {
+		
+		class SourcePosition;
+		
+	}
+	
+	namespace Parser {
+		
+		class TokenReader;
+		
+		class ValueBuilder {
+		public:
+			ValueBuilder(const TokenReader& reader);
+			~ValueBuilder();
+			
+			AST::Node<AST::Value>
+			makeTernaryValue(AST::Node<AST::Value> conditionValue,
+			                 AST::Node<AST::Value> ifTrueValue,
+			                 AST::Node<AST::Value> ifFalseValue,
+			                 const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Value>
+			makeLogicalOrValue(AST::Node<AST::Value> leftValue,
+			                   AST::Node<AST::Value> rightValue,
+			                   const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Value>
+			makeLogicalAndValue(AST::Node<AST::Value> leftValue,
+			                    AST::Node<AST::Value> rightValue,
+			                    const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Value>
+			makeBitwiseOrValue(AST::Node<AST::Value> leftValue,
+			                   AST::Node<AST::Value> rightValue,
+			                   const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Value>
+			makeBitwiseXorValue(AST::Node<AST::Value> leftValue,
+			                    AST::Node<AST::Value> rightValue,
+			                    const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Value>
+			makeBitwiseAndValue(AST::Node<AST::Value> leftValue,
+			                    AST::Node<AST::Value> rightValue,
+			                    const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Value>
+			makeBinaryOpValue(AST::Node<AST::Value> leftValue,
+			                  AST::Node<AST::Value> rightValue,
+			                  AST::BinaryOpKind opKind,
+			                  const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Value>
+			makeUnaryOpValue(AST::Node<AST::Value> operand,
+			                 AST::UnaryOpKind opKind,
+			                 const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Value>
+			makeCallValue(AST::Node<AST::Value> callableValue,
+			              const AST::Node<AST::ValueList>& parameters,
+			              const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Value>
+			makeIndexValue(AST::Node<AST::Value> value,
+			               AST::Node<AST::Value> indexValue,
+			               const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Value>
+			makeLiteralValue(Constant constant, String literalSpecifier,
+			                 const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Value>
+			makeSelfValue(const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Value>
+			makeThisValue(const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Value>
+			makeAlignOfValue(AST::Node<AST::Value> operand,
+			                 const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Value>
+			makeSizeOfValue(AST::Node<AST::Value> operand,
+			                const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Value>
+			makeSelfMemberAccess(String name,
+			                     const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Value>
+			makeArrayLiteralValue(AST::Node<AST::ValueList> values,
+			                      const Debug::SourcePosition& start);
+			
+			AST::Node<AST::ValueList>
+			makeValueList(AST::ValueList values,
+			              const Debug::SourcePosition& start);
+			
+		private:
+			TokenReader& reader_;
+			
+		};
+		
+	}
+	
+}
+
+#endif
