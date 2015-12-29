5bde3c60406c505b6426be0e8a950eb02db9e274
Stephen Cross <scross@scross.co.uk>
2015-12-29 16:41:18 +0000

    Added implementation of SymbolBuilder.

---
+++ lib/Parser/SymbolBuilder.cpp
@@ -0,0 +1,45 @@
+#include <locic/AST.hpp>
+#include <locic/Debug/SourcePosition.hpp>
+#include <locic/Parser/SymbolBuilder.hpp>
+#include <locic/Parser/TokenReader.hpp>
+#include <locic/Support/String.hpp>
+
+namespace locic {
+	
+	namespace Parser {
+		
+		SymbolBuilder::SymbolBuilder(const TokenReader& reader)
+		: reader_(reader) { }
+		
+		SymbolBuilder::~SymbolBuilder() { }
+		
+		AST::Node<AST::Symbol>
+		SymbolBuilder::makeSymbolNode(AST::Symbol symbol,
+		                              const Debug::SourcePosition& start) {
+			const auto location = reader_.locationWithRangeFrom(start);
+			return AST::makeNode(location, new AST::Symbol(symbol));
+		}
+		
+		AST::Node<AST::SymbolElement>
+		SymbolBuilder::makeSymbolElement(const String name,
+		                                 AST::Node<AST::ValueList> templateArguments,
+		                                 const Debug::SourcePosition& start) {
+			const auto location = reader_.locationWithRangeFrom(start);
+			return AST::makeNode(location, new AST::SymbolElement(name, templateArguments));
+		}
+		
+		AST::Node<AST::SymbolElement>
+		SymbolBuilder::makeTypenameSymbolElement(const Debug::SourcePosition& /*start*/) {
+			throw std::logic_error("TODO");
+		}
+		
+		AST::Node<AST::ValueList>
+		SymbolBuilder::makeValueList(AST::ValueList values,
+		                             const Debug::SourcePosition& start) {
+			const auto location = reader_.locationWithRangeFrom(start);
+			return AST::makeNode(location, new AST::ValueList(std::move(values)));
+		}
+		
+	}
+	
+}
