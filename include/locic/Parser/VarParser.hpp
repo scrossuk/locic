fb00fd0bfcba0ac7a90593d0a1f75235a56256dc
Stephen Cross <scross@scross.co.uk>
2015-12-29 23:11:00 +0000

    Added VarParser class API.

--- include/locic/Parser/VarParser.hpp
+++ include/locic/Parser/VarParser.hpp
@@ -2,8 +2,16 @@
 #define LOCIC_PARSER_VARPARSER_HPP
 
 #include <locic/AST.hpp>
+#include <locic/Parser/Token.hpp>
+#include <locic/Parser/VarBuilder.hpp>
 
 namespace locic {
+	
+	namespace Debug {
+		
+		class SourcePosition;
+		
+	}
 	
 	namespace Parser {
 		
@@ -16,8 +24,17 @@ namespace locic {
 			
 			AST::Node<AST::TypeVar> parseVar();
 			
+			bool scanOptionalToken(Token::Kind kind);
+			
+			AST::Node<AST::TypeVar> parseTypeVar();
+			
+			AST::Node<AST::TypeVar>
+			parseTypeVarWithType(AST::Node<AST::Type> type,
+			                     const Debug::SourcePosition& start);
+			
 		private:
 			TokenReader& reader_;
+			VarBuilder builder_;
 			
 		};
 		
