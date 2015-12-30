e13c9f0a34d4730c4ee33a6e3b87b495b59bc004
Stephen Cross <scross@scross.co.uk>
2015-12-30 23:42:17 +0000

    Added method declarations for creating type instances.

--- include/locic/Parser/TypeInstanceBuilder.hpp
+++ include/locic/Parser/TypeInstanceBuilder.hpp
@@ -20,6 +20,32 @@ namespace locic {
 			TypeInstanceBuilder(const TokenReader& reader);
 			~TypeInstanceBuilder();
 			
+			AST::Node<AST::TypeInstance>
+			makeClassDecl(String name, AST::Node<AST::FunctionList> methods,
+			              const Debug::SourcePosition& start);
+			
+			AST::Node<AST::TypeInstance>
+			makeClassDef(String name, AST::Node<AST::TypeVarList> variables,
+			             AST::Node<AST::FunctionList> methods,
+			             const Debug::SourcePosition& start);
+			
+			AST::Node<AST::TypeInstance>
+			makeDatatype(String name, AST::Node<AST::TypeVarList> variables,
+			             const Debug::SourcePosition& start);
+			
+			AST::Node<AST::TypeInstance>
+			makeUnionDatatype(String name, AST::Node<AST::TypeInstanceList> variants,
+			                  const Debug::SourcePosition& start);
+			
+			AST::Node<AST::TypeInstance>
+			makeException(String name, AST::Node<AST::TypeVarList> variables,
+			              AST::Node<AST::ExceptionInitializer> initializer,
+			              const Debug::SourcePosition& start);
+			
+			AST::Node<AST::TypeInstance>
+			makeInterface(String name, AST::Node<AST::FunctionList> methods,
+			              const Debug::SourcePosition& start);
+			
 		private:
 			const TokenReader& reader_;
 			
