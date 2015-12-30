047cb8a4a7d8c6d343cc852e344213a18c23b6ad
Stephen Cross <scross@scross.co.uk>
2015-12-30 16:47:52 +0000

    Added initial code for parsing namespaces.

---
+++ lib/Parser/NamespaceParser.cpp
@@ -0,0 +1,119 @@
+#include <locic/AST.hpp>
+#include <locic/Parser/Diagnostics.hpp>
+#include <locic/Parser/FunctionParser.hpp>
+#include <locic/Parser/NamespaceBuilder.hpp>
+#include <locic/Parser/NamespaceParser.hpp>
+#include <locic/Parser/Token.hpp>
+#include <locic/Parser/TokenReader.hpp>
+#include <locic/Parser/TypeInstanceParser.hpp>
+
+namespace locic {
+	
+	class StringHost;
+	
+	namespace Parser {
+		
+		NamespaceParser::NamespaceParser(TokenReader& reader)
+		: reader_(reader), builder_(reader) { }
+		
+		NamespaceParser::~NamespaceParser() { }
+		
+		AST::Node<AST::NamespaceDecl> NamespaceParser::parseNamespace() {
+			const auto start = reader_.position();
+			
+			reader_.expect(Token::NAMESPACE);
+			
+			const auto name = reader_.expectName();
+			
+			reader_.expect(Token::LCURLYBRACKET);
+			
+			const auto namespaceData = parseNamespaceData();
+			
+			reader_.expect(Token::RCURLYBRACKET);
+			
+			return builder_.makeNamespace(name, namespaceData, start);
+		}
+		
+		AST::Node<AST::NamespaceData> NamespaceParser::parseNamespaceData() {
+			const auto start = reader_.position();
+			
+			AST::NamespaceData data;
+			
+			while (true) {
+				while (reader_.peek().kind() == Token::SEMICOLON) {
+					reader_.consume();
+				}
+				
+				if (reader_.peek().kind() == Token::RCURLYBRACKET) {
+					break;
+				}
+				
+				switch (reader_.peek().kind()) {
+					case Token::TEMPLATE:
+						parseTemplatedObject(data);
+						break;
+					case Token::USING:
+						//data.aliases.push_back(parseAlias());
+						throw std::logic_error("TODO: parse alias");
+						break;
+					case Token::STATIC:
+						//data.staticAsserts.push_back(parseStaticAssert());
+						throw std::logic_error("TODO: parse static assert");
+						break;
+					case Token::CLASS:
+					case Token::DATATYPE:
+					case Token::EXCEPTION:
+					case Token::INTERFACE:
+					case Token::STRUCT:
+					case Token::UNION: {
+						auto typeInstance = TypeInstanceParser(reader_).parseTypeInstance();
+						data.typeInstances.push_back(typeInstance);
+						break;
+					}
+					case Token::IMPORT:
+					case Token::EXPORT:
+						data.moduleScopes.push_back(parseModuleScope());
+						break;
+					case Token::NAMESPACE:
+						data.namespaces.push_back(parseNamespace());
+						break;
+					default: {
+						auto function = FunctionParser(reader_).parseGlobalFunction();
+						data.functions.push_back(function);
+						break;
+					}
+				}
+			}
+			
+			return builder_.makeNamespaceData(data, start);
+		}
+		
+		void NamespaceParser::parseTemplatedObject(AST::NamespaceData& /*data*/) {
+			throw std::logic_error("TODO: parse templated object");
+		}
+		
+		AST::Node<AST::ModuleScope> NamespaceParser::parseModuleScope() {
+			const auto start = reader_.position();
+			
+			const auto token = reader_.expectOneOf({ Token::EXPORT, Token::IMPORT });
+			
+			if (reader_.peek().kind() == Token::LCURLYBRACKET) {
+				reader_.consume();
+				
+				const auto data = parseNamespaceData();
+				
+				reader_.expect(Token::RCURLYBRACKET);
+				
+				if (token.kind() == Token::EXPORT) {
+					return builder_.makeUnnamedExport(data, start);
+				} else {
+					return builder_.makeUnnamedImport(data, start);
+				}
+			}
+			
+			throw std::logic_error("TODO: parse named module scope");
+		}
+		
+	}
+	
+}
