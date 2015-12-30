85fc31c07f7ebaf616048ceb758ac7b20182132b
Stephen Cross <scross@scross.co.uk>
2015-12-30 23:44:01 +0000

    Added initial code for parsing type instances.

---
+++ lib/Parser/TypeInstanceParser.cpp
@@ -0,0 +1,122 @@
+#include <locic/AST.hpp>
+#include <locic/Parser/Diagnostics.hpp>
+#include <locic/Parser/Token.hpp>
+#include <locic/Parser/TokenReader.hpp>
+#include <locic/Parser/TypeInstanceBuilder.hpp>
+#include <locic/Parser/TypeInstanceParser.hpp>
+#include <locic/Parser/VarParser.hpp>
+
+namespace locic {
+	
+	class StringHost;
+	
+	namespace Parser {
+		
+		TypeInstanceParser::TypeInstanceParser(TokenReader& reader)
+		: reader_(reader), builder_(reader) { }
+		
+		TypeInstanceParser::~TypeInstanceParser() { }
+		
+		AST::Node<AST::TypeInstance> TypeInstanceParser::parseTypeInstance() {
+			const auto token = reader_.peek();
+			switch (token.kind()) {
+				case Token::CLASS:
+					return parseClass();
+				case Token::DATATYPE:
+					return parseDatatype();
+				case Token::EXCEPTION:
+					return parseException();
+				case Token::INTERFACE:
+					return parseInterface();
+				case Token::STRUCT:
+					return parseStruct();
+				case Token::UNION:
+					return parseUnion();
+				default:
+					throw std::logic_error("TODO: invalid type instance");
+			}
+		}
+		
+		AST::Node<AST::TypeInstance> TypeInstanceParser::parseClass() {
+			const auto start = reader_.position();
+			
+			reader_.expect(Token::CLASS);
+			const auto name = reader_.expectName();
+			
+			if (reader_.peek().kind() == Token::LCURLYBRACKET) {
+				reader_.consume();
+				const auto methods = parseMethodDeclList();
+				reader_.expect(Token::RCURLYBRACKET);
+				return builder_.makeClassDecl(name, methods, start);
+			}
+			
+			reader_.expect(Token::LROUNDBRACKET);
+			const auto varList = VarParser(reader_).parseVarList();
+			reader_.expect(Token::RROUNDBRACKET);
+			
+			reader_.expect(Token::LCURLYBRACKET);
+			const auto methods = parseMethodDefList();
+			reader_.expect(Token::RCURLYBRACKET);
+			
+			return builder_.makeClassDef(name, varList, methods, start);
+		}
+		
+		AST::Node<AST::TypeInstance> TypeInstanceParser::parseDatatype() {
+			const auto start = reader_.position();
+			
+			reader_.expect(Token::DATATYPE);
+			const auto name = reader_.expectName();
+			
+			if (reader_.peek().kind() == Token::LROUNDBRACKET) {
+				reader_.consume();
+				const auto varList = VarParser(reader_).parseVarList();
+				reader_.expect(Token::RROUNDBRACKET);
+				return builder_.makeDatatype(name, varList, start);
+			}
+			
+			reader_.expect(Token::SETEQUAL);
+			
+			const auto variants = parseDatatypeVariantList();
+			
+			return builder_.makeUnionDatatype(name, variants, start);
+		}
+		
+		AST::Node<AST::TypeInstance> TypeInstanceParser::parseException() {
+			const auto start = reader_.position();
+			
+			reader_.expect(Token::EXCEPTION);
+			const auto name = reader_.expectName();
+			
+			reader_.expect(Token::LROUNDBRACKET);
+			const auto varList = VarParser(reader_).parseVarList();
+			reader_.expect(Token::RROUNDBRACKET);
+			
+			const auto initializer = parseExceptionInitializer();
+			
+			return builder_.makeException(name, varList, initializer, start);
+		}
+		
+		AST::Node<AST::TypeInstance> TypeInstanceParser::parseInterface() {
+			const auto start = reader_.position();
+			
+			reader_.expect(Token::INTERFACE);
+			const auto name = reader_.expectName();
+			
+			reader_.expect(Token::LCURLYBRACKET);
+			const auto methods = parseMethodDeclList();
+			reader_.expect(Token::RCURLYBRACKET);
+			
+			return builder_.makeInterface(name, methods, start);
+		}
+		
+		AST::Node<AST::TypeInstance> TypeInstanceParser::parseStruct() {
+			throw std::logic_error("TODO: parse struct");
+		}
+		
+		AST::Node<AST::TypeInstance> TypeInstanceParser::parseUnion() {
+			throw std::logic_error("TODO: parse union");
+		}
+		
+	}
+	
+}
