8e2b2719f321758e349a46ea1c6cb04ba8704e90
Stephen Cross <scross@scross.co.uk>
2015-12-30 16:46:56 +0000

    Added initial FunctionParser API.

---
+++ include/locic/Parser/FunctionParser.hpp
@@ -0,0 +1,48 @@
+#ifndef LOCIC_PARSER_FUNCTIONPARSER_HPP
+#define LOCIC_PARSER_FUNCTIONPARSER_HPP
+
+#include <locic/AST.hpp>
+#include <locic/Parser/FunctionBuilder.hpp>
+
+namespace locic {
+	
+	namespace Debug {
+		
+		class SourcePosition;
+		
+	}
+	
+	namespace Parser {
+		
+		class TokenReader;
+		
+		class FunctionParser {
+		public:
+			FunctionParser(TokenReader& reader);
+			~FunctionParser();
+			
+			AST::Node<AST::Function> parseGlobalFunction();
+			
+			AST::Node<AST::Function>
+			parseBasicFunction(const Debug::SourcePosition& start);
+			
+			AST::Node<AST::RequireSpecifier>
+			parseOptionalConstSpecifier();
+			
+			AST::Node<AST::RequireSpecifier>
+			parseOptionalNoexceptSpecifier();
+			
+			AST::Node<AST::RequireSpecifier>
+			parseOptionalRequireSpecifier();
+			
+		private:
+			TokenReader& reader_;
+			FunctionBuilder builder_;
+			
+		};
+		
+	}
+	
+}
+
+#endif
