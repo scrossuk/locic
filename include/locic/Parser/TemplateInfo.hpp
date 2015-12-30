c9af36e9c53be58471baf8889635df309b57e33d
Stephen Cross <scross@scross.co.uk>
2015-12-30 22:06:42 +0000

    Added TemplateInfo class.

---
+++ include/locic/Parser/TemplateInfo.hpp
@@ -0,0 +1,42 @@
+#ifndef LOCIC_PARSER_TEMPLATEINFO_HPP
+#define LOCIC_PARSER_TEMPLATEINFO_HPP
+
+#include <locic/AST.hpp>
+#include <locic/Support/Optional.hpp>
+
+namespace locic {
+	
+	namespace Parser {
+		
+		class TemplateInfo {
+		public:
+			TemplateInfo();
+			~TemplateInfo();
+			
+			AST::Node<AST::TemplateTypeVarList> templateVariables() const;
+			AST::Node<AST::RequireSpecifier> requireSpecifier() const;
+			AST::Node<AST::RequireSpecifier> moveSpecifier() const;
+			AST::Node<AST::StringList> noTagSet() const;
+			
+			bool hasRequireSpecifier() const;
+			bool hasMoveSpecifier() const;
+			bool hasNoTagSet() const;
+			
+			void setTemplateVariables(AST::Node<AST::TemplateTypeVarList> templateVariables);
+			void setRequireSpecifier(AST::Node<AST::RequireSpecifier> specifier);
+			void setMoveSpecifier(AST::Node<AST::RequireSpecifier> specifier);
+			void setNoTagSet(AST::Node<AST::StringList> noTagSet);
+			
+		private:
+			Optional<AST::Node<AST::TemplateTypeVarList>> templateVariables_;
+			Optional<AST::Node<AST::RequireSpecifier>> requireSpecifier_;
+			Optional<AST::Node<AST::RequireSpecifier>> moveSpecifier_;
+			Optional<AST::Node<AST::StringList>> noTagSet_;
+			
+		};
+		
+	}
+	
+}
+
+#endif
