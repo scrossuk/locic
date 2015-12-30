6911f6aacae71f3ea06f6f475ccebf7ac4554e20
Stephen Cross <scross@scross.co.uk>
2015-12-30 16:47:07 +0000

    Added initial NamespaceParser API.

---
+++ include/locic/Parser/NamespaceParser.hpp
@@ -0,0 +1,42 @@
+#ifndef LOCIC_PARSER_NAMESPACEPARSER_HPP
+#define LOCIC_PARSER_NAMESPACEPARSER_HPP
+
+#include <locic/AST.hpp>
+#include <locic/Parser/NamespaceBuilder.hpp>
+
+namespace locic {
+	
+	namespace Debug {
+		
+		class SourcePosition;
+		
+	}
+	
+	namespace Parser {
+		
+		class TokenReader;
+		
+		class NamespaceParser {
+		public:
+			NamespaceParser(TokenReader& reader);
+			~NamespaceParser();
+			
+			AST::Node<AST::NamespaceDecl> parseNamespace();
+			
+			AST::Node<AST::NamespaceData> parseNamespaceData();
+			
+			void parseTemplatedObject(AST::NamespaceData& data);
+			
+			AST::Node<AST::ModuleScope> parseModuleScope();
+			
+		private:
+			TokenReader& reader_;
+			NamespaceBuilder builder_;
+			
+		};
+		
+	}
+	
+}
+
+#endif
