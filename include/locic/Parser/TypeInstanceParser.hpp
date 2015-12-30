3f5f47c92f22a7f090729d1c649b69f02b2f6f2f
Stephen Cross <scross@scross.co.uk>
2015-12-30 23:43:08 +0000

    Added method declarations for TypeInstanceParser.

--- include/locic/Parser/TypeInstanceParser.hpp
+++ include/locic/Parser/TypeInstanceParser.hpp
@@ -23,6 +23,26 @@ namespace locic {
 			
 			AST::Node<AST::TypeInstance> parseTypeInstance();
 			
+			AST::Node<AST::TypeInstance> parseClass();
+			
+			AST::Node<AST::TypeInstance> parseDatatype();
+			
+			AST::Node<AST::TypeInstanceList> parseDatatypeVariantList();
+			
+			AST::Node<AST::TypeInstance> parseException();
+			
+			AST::Node<AST::ExceptionInitializer> parseExceptionInitializer();
+			
+			AST::Node<AST::TypeInstance> parseInterface();
+			
+			AST::Node<AST::TypeInstance> parseStruct();
+			
+			AST::Node<AST::TypeInstance> parseUnion();
+			
+			AST::Node<AST::FunctionList> parseMethodDeclList();
+			
+			AST::Node<AST::FunctionList> parseMethodDefList();
+			
 		private:
 			TokenReader& reader_;
 			TypeInstanceBuilder builder_;
