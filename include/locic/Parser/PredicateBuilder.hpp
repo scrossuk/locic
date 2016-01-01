29b8c2d3866bbde61d4951a968d252ff7f269a51
Stephen Cross <scross@scross.co.uk>
2016-01-01 16:19:09 +0000

    Added PredicateBuilder class declaration.

---
+++ include/locic/Parser/PredicateBuilder.hpp
@@ -0,0 +1,65 @@
+#ifndef LOCIC_PARSER_PREDICATEBUILDER_HPP
+#define LOCIC_PARSER_PREDICATEBUILDER_HPP
+
+#include <locic/AST.hpp>
+
+namespace locic {
+	
+	namespace Debug {
+		
+		class SourcePosition;
+		
+	}
+	
+	namespace Parser {
+		
+		class TokenReader;
+		
+		class PredicateBuilder {
+		public:
+			PredicateBuilder(const TokenReader& reader);
+			~PredicateBuilder();
+			
+			AST::Node<AST::Predicate>
+			makePredicateNode(AST::Predicate* predicate,
+			                  const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Predicate>
+			makeTruePredicate(const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Predicate>
+			makeFalsePredicate(const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Predicate>
+			makeBracketPredicate(AST::Node<AST::Predicate> predicate,
+			                     const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Predicate>
+			makeTypeSpecPredicate(AST::Node<AST::Type> type,
+			                      AST::Node<AST::Type> capabilityType,
+			                      const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Predicate>
+			makeSymbolPredicate(AST::Node<AST::Symbol> symbol,
+			                    const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Predicate>
+			makeAndPredicate(AST::Node<AST::Predicate> leftPredicate,
+			                 AST::Node<AST::Predicate> rightPredicate,
+			                 const Debug::SourcePosition& start);
+			
+			AST::Node<AST::Predicate>
+			makeOrPredicate(AST::Node<AST::Predicate> leftPredicate,
+			                AST::Node<AST::Predicate> rightPredicate,
+			                const Debug::SourcePosition& start);
+			
+		private:
+			const TokenReader& reader_;
+			
+		};
+		
+	}
+	
+}
+
+#endif
