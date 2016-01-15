c77106758e611a16ffc8c1d826ea40a02e15a0e4
Stephen Cross <scross@scross.co.uk>
2016-01-15 22:10:14 +0000

    Added Diag::lexId() for accessing Lex diag kind.

--- include/locic/Frontend/Diagnostics.hpp
+++ include/locic/Frontend/Diagnostics.hpp
@@ -3,8 +3,14 @@
 
 #include <string>
 
+#include <locic/Support/ErrorHandling.hpp>
+
 namespace locic {
-		
+	
+	namespace Lex {
+		enum class DiagID;
+	}
+	
 	enum class DiagLevel {
 		Error,
 		Warning,
@@ -16,6 +22,10 @@ namespace locic {
 		virtual ~Diag() { }
 		
 		virtual DiagLevel level() const = 0;
+		
+		virtual Lex::DiagID lexId() const {
+			locic_unreachable("Not lex diagnostic.");
+		}
 		
 		virtual std::string toString() const = 0;
 		
